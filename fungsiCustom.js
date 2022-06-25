b88e6b790abe0cb2c3d3656979e43e5c0df75830
Irvan Wijaya <irvanwijaya72@gmail.com>
2022-06-25 13:45:25 +0700

    membuat sebuah callback bacaData di fungsiCustom.js

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,59 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (cb) => {
+
+  let arr = [];
+
+  fs.readFile(
+    file1, {encoding: 'utf-8'}, (err,data) => {
+      if (err){throw err;}
+
+      const dataJson = JSON.parse(data)
+      arr.push(dataJson.message.split(" ")[1])
+
+      // cb(err,arr)
+    }
+  )
+fs.readFile(
+    file2, {encoding: 'utf-8'}, (err,data) => {
+      if (err){throw err;}
+
+      const dataJson = JSON.parse(data)[0]
+      arr.push(dataJson.message.split(" ")[1])
+
+      // cb(err,arr)
+    }
+  )
+  fs.readFile(
+    file3, {encoding: 'utf-8'}, (err,data) => {
+      if (err){throw err;}
+
+      const dataJson = JSON.parse(data)[0]
+      arr.push(dataJson.data.message.split(" ")[1])
+
+      cb(err,arr)
+    }
+  )
+
+  // let arr = []
+  // fs.readFile(file1,(err,data)=> {
+  //   if (err) return cb(err,null);
+  //   arr.push(JSON.parse(data).message.split(" ")[1]); 
+  
+  // fs.readFile(file2,(err,data)=> {
+  //   if (err) return cb(err,null);
+  //   arr.push(JSON.parse(data)[0].message.split(" ")[1])
+  
+  // fs.readFile(file3,(err,data)=> {
+  //   if (err) return cb(err,null);
+  //   arr.push(JSON.parse(data)[0].data.message.split(" ")[1])
+
+  //   cb(err,arr)
+  // })
+  // })
+  // })
+}
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
